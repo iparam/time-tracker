a114d3ec3da3d99945619641b7915518f6ccc86a
Param <iparamtech@gmail.com>
2011-12-03 21:08:35 +0530

    removed temp file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111127141600) do
+ActiveRecord::Schema.define(:version => 20111201102834) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -88,6 +88,10 @@ ActiveRecord::Schema.define(:version => 20111127141600) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "account_id"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "full_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
